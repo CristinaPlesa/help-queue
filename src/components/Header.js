83f458039592c69f3e7fe7d4d86748a1fc59446f
Cristina Plesa <cristinaplesa8@gmail.com>
2021-06-30 23:22:24 -0700

    add form code and event handler to NewTicketForm

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ function Header(){
   return (
     <React.Fragment>
       <h1>Help Queue</h1>
-      <img src={ticketsImage} alt="An image of a ticket" />
+      <img width="50%" src={ticketsImage} alt="An image of a ticket" />
     </React.Fragment>
   );
 }
