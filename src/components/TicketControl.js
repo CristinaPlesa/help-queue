19097ca9b5923e0829060905892cd27be816bd88
Cristina Plesa <cristinaplesa8@gmail.com>
2021-06-30 23:56:07 -0700

    set up masterTicketList in state through TicketList callback'

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -7,33 +7,34 @@ class TicketControl extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      formVisibleOnPage: false
+      formVisibleOnPage: false,
+      masterTicketList: [] // new code
     };
-    this.handleClick = this.handleClick.bind(this); //new code here
+    // this.handleClick = this.handleClick.bind(this); //new code here
   }
 
-  handleClick() {
-    this.setState(prevState => ({
-      formVisibleOnPage: !prevState.formVisibleOnPage
-    }));
-  }
-
-  // this works too. Has arrow notation, so bind() would not be needed to recognize the lexical scope of this.
-  // handleClick = () => {
+  // handleClick() {
   //   this.setState(prevState => ({
   //     formVisibleOnPage: !prevState.formVisibleOnPage
   //   }));
   // }
+
+  // this works too. Has arrow notation, so bind() would not be needed to recognize the lexical scope of this.
+  handleClick = () => {
+    this.setState(prevState => ({
+      formVisibleOnPage: !prevState.formVisibleOnPage
+    }));
+  }
 
   render(){
     let currentlyVisibleState = null;
     let buttonText = null; // new code
     if (this.state.formVisibleOnPage) {
       currentlyVisibleState = <NewTicketForm />;
-      buttonText = "Return to Ticket List"; // new code
+      buttonText = "Return to Ticket List";
     } else {
-      currentlyVisibleState = <TicketList />;
-      buttonText = "Add Ticket"; // new code
+      currentlyVisibleState = <TicketList ticketList={this.state.masterTicketList} />; // new code
+      buttonText = "Add Ticket"; 
     }
     return (
       <React.Fragment>
