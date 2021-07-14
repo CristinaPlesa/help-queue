c60b72f137e2f0f8591824b6ac8b3f3b4da08f55
Cristina Plesa <cristinaplesa8@gmail.com>
2021-07-13 23:46:29 -0700

    add authorization and authentication through firebase

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,20 @@
 import React from "react";
 import ticketsImage from "./../img/ticket.jpg";
+import { Link } from "react-router-dom";
 
 function Header(){
   return (
     <React.Fragment>
-      <h1>Help Queue</h1>
+      <h1> Help Queue</h1>
       <img width="50%" src={ticketsImage} alt="An image of a ticket" />
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/signin">Sign In</Link>
+        </li>
+      </ul>
     </React.Fragment>
   );
 }
